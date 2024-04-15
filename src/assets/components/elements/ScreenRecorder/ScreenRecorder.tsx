f3b8e48db5819e7bc835b0442fe6dc1ec5134d06
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-04-15 10:57:10 -0300

    feat: :lipstick: Modifying styles.

--- src/assets/components/elements/ScreenRecorder/ScreenRecorder.tsx
+++ src/assets/components/elements/ScreenRecorder/ScreenRecorder.tsx
@@ -17,8 +17,12 @@ const ScreenRecorder = ({
     <div className={styles.screenRecorder}>
       <video src={mediaBlobUrl} controls autoPlay loop />
       <div className={styles.screenRecorder__buttons}>
-        <button onClick={startRecording}>Start</button>
-        <button onClick={stopRecording}>Stop</button>
+        <button className={styles.start} onClick={startRecording}>
+          START
+        </button>
+        <button className={styles.stop} onClick={stopRecording}>
+          STOP
+        </button>
       </div>
       <p>{status}</p>
     </div>
